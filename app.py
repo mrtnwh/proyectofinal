4782b0fb30484b966eac83efdafbc8795576167e
tatianagutierrez <tatiana7gutierrez@gmail.com>
2022-01-29 20:05:36 -0300

    agregue endpoints directores y generos

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request
 import json, urllib.request
 
 # API MOCKACHINO
-response = ""
 
+    #Usuarios
 handlerApiUsuarios = urllib.request.urlopen("https://www.mockachino.com/e87585d1-9630-4f/usuarios")
+response = ''
 
-for linea in handlerApiUsuarios:
+for linea in handlerApiUsuarios:    # OPTIMIZAR
     response += linea.decode()
 
 listaUsuarios = json.loads(response)["usuarios"]
@@ -31,6 +32,51 @@ def login():
                     return render_template("index.html")
     
     return render_template("login.html")
+
+@app.route("/directores")
+def getDirectores():
+    response = ''
+    handler = urllib.request.urlopen("https://www.mockachino.com/e87585d1-9630-4f/directores")
+    
+    for linea in handler:
+        response += linea.decode()
+
+    return json.loads(response)
+
+@app.route("/generos")
+def getGeneros():
+    response = ''
+    handler = urllib.request.urlopen("https://www.mockachino.com/e87585d1-9630-4f/generos")
+    
+    for linea in handler:
+        response += linea.decode()
+
+    return json.loads(response)
+
+
+""" @app.route("/subir_pelicula", methods=["POST", "GET"])
+def subir_pelicula():
+    if request.method == "POST":
+        ultimoId = listaPeliculas[-1]['id']
+
+        data = jsonify({
+            "id": ultimoId, 
+            "title": request.form["title"],
+            "director": request.form["director"],
+            "date": request.form["date"],
+            "poster": request.form["poster"],
+            "overview": request.form["overview"],
+            "genre": [
+                    {
+                        "id":0,
+                        "name": request.form["genre"]
+                    }
+            ],
+        })
+    else:
+        return render_template("subir_pelicula.html")
+
+    return render_template("subir_pelicula.html") """
 
 if __name__ == "__main__":
     app.run()
