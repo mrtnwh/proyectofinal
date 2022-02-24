ca2753f69c7676959d3b86b64196a759144a6c89
mrtnwh <lagonegromartin@gmail.com>
2022-02-24 20:31:53 -0300

    correcion de carousel con las 10 imagenes del json

--- static/js/carrousel.js
+++ static/js/carrousel.js
@@ -1,8 +1,29 @@
 const fila = document.querySelector(".contenedor-carousel");
+const carousel = document.querySelector(".carousel");
 const peliculas = document.querySelectorAll(".pelicula");
 
 const flechaIzquierda = document.getElementById("flecha-izquierda");
 const flechaDerecha = document.getElementById("flecha-derecha");
+
+function agregarPeliculas() {
+  fetch("../json/peliculas.json")
+    .then((respuesta) => respuesta.json()) // indicar el formato que queremos que se obtenga la informacion
+    .then((peliculas) => {
+      peliculas.forEach((pelicula) => {
+        const pelis = document.createElement("div");
+        pelis.innerHTML += `
+        <a href="#">
+        <br>
+        <p class="pelicula-titulo"> ${pelicula.title} </p> <img src="${pelicula.poster}" class="image">
+        </a>
+`;
+        carousel.appendChild(pelis);
+      });
+    })
+    .catch((error) => console.log("Hubo un error : " + error.message));
+}
+
+agregarPeliculas();
 
 // ? ----- ----- Event Listener para la flecha derecha. ----- -----
 flechaDerecha.addEventListener("click", () => {
@@ -24,37 +45,4 @@ flechaIzquierda.addEventListener("click", () => {
     indicadorActivo.previousSibling.classList.add("activo");
     indicadorActivo.classList.remove("activo");
   }
-});
-
-// ? ----- ----- Paginacion ----- -----
-const numeroPaginas = Math.ceil(peliculas.length / 5);
-for (let i = 0; i < numeroPaginas; i++) {
-  const indicador = document.createElement("button");
-
-  if (i === 0) {
-    indicador.classList.add("activo");
-  }
-
-  document.querySelector(".indicadores").appendChild(indicador);
-  indicador.addEventListener("click", (e) => {
-    fila.scrollLeft = i * fila.offsetWidth;
-
-    document.querySelector(".indicadores .activo").classList.remove("activo");
-    e.target.classList.add("activo");
-  });
-}
-
-// ? ----- ----- Hover ----- -----
-peliculas.forEach((pelicula) => {
-  pelicula.addEventListener("mouseenter", (e) => {
-    const elemento = e.currentTarget;
-    setTimeout(() => {
-      peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
-      elemento.classList.add("hover");
-    }, 300);
-  });
-});
-
-fila.addEventListener("mouseleave", () => {
-  peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
 });
