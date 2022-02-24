dd3ddcdf7e13025310513f743eaa9752e45315ae
tatianagutierrez <tatiana7gutierrez@gmail.com>
2022-02-24 18:07:23 -0300

    implemente session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import json, urllib.request
 
 # API MOCKACHINO
@@ -22,6 +22,7 @@ data = getJsonPeliculas()
 
 # FLASK
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'C52zMh3cmKb5UvPg'
 
 @app.route("/")
 def index():
@@ -33,9 +34,9 @@ def login():
 
     if request.method == "POST":
         for usr in listaUsuarios:
-            if request.form["email"] == usr["email"]:
-                if request.form["password"] == usr["password"]:
-                    return render_template("index.html")
+            if request.form["email"] == usr["email"] and request.form["password"] == usr["password"]:
+                session['logeado'] = True
+                return render_template("index.html")
     
     return render_template("login.html")
 
@@ -72,35 +73,38 @@ def peliculasConPortada():
 @app.route("/subir_pelicula", methods=["POST", "GET"])
 def subir_pelicula():
 
-    if request.method == "POST":
-        ultimoId = data["peliculas"][-1]["id"]
+    if not session.get('logeado'):
+        return render_template("login.html")
+    else:
 
-        # Crear if en caso de que no se haya adjuntado un poster, subir img por default
-        if request.form["poster"] == "" :
-            poster = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
-        else:
-            poster = request.form["poster"]
+        if request.method == "POST":
+            ultimoId = data["peliculas"][-1]["id"]
 
-        pelicula = {
-            "id": ultimoId + 1,
-            "title": request.form["title"],
-            "director": request.form["director"],
-            "date": request.form["date"],
-            "poster": poster,
-            "overview": request.form["overview"],
-            "genre": request.form["genre"],
-            "vote_average": 0,
-            "vote_count": 0
-        }
+            if request.form["poster"] == "" :
+                poster = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+            else:
+                poster = request.form["poster"]
 
-        data["peliculas"].append(pelicula)
+            pelicula = {
+                "id": ultimoId + 1,
+                "title": request.form["title"],
+                "director": request.form["director"],
+                "date": request.form["date"],
+                "poster": poster,
+                "overview": request.form["overview"],
+                "genre": request.form["genre"],
+                "vote_average": 0,
+                "vote_count": 0
+            }
 
-        with open('static/json/peliculas.json', 'w') as file:
-            json.dump(data, file, indent=4)
+            data["peliculas"].append(pelicula)
 
-        return render_template("peliculas.html")
+            with open('static/json/peliculas.json', 'w') as file:
+                json.dump(data, file, indent=4)
 
-    return render_template("subir_pelicula.html")
+            return render_template("peliculas.html")
+
+        return render_template("subir_pelicula.html")
 
 @app.route("/peliculas")
 def peliculas():
@@ -113,13 +117,6 @@ def generos():
 @app.route("/generos/<genero>")
 def peliPorGenero(genero):
     genero = genero.capitalize()
-
-    """ listaFiltradas = list()
-
-    for pelicula in data["peliculas"]:
-        if genero == pelicula["genre"]:
-            listaFiltradas.append(pelicula) """
-
     return render_template("peliculas_genero.html", genero = genero)
 
 if __name__ == "__main__":
