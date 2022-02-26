86d329452125c51f000686e57b29a30262aa7864
mrtnwh <lagonegromartin@gmail.com>
2022-02-26 19:46:04 -0300

    añadido del iframe para trailer de cada pelicula

--- static/js/pelicula_info.js
+++ static/js/pelicula_info.js
@@ -1,28 +1,30 @@
-var idPelicula = document.getElementsByClassName('cont-info')[0].id;
-var contInfoDcha = document.getElementById('cont-info-dcha');
-var row1 = document.getElementById('row1');
-var row2 = document.getElementById('row2')
+var idPelicula = document.getElementsByClassName("cont-info")[0].id;
+var contInfoDcha = document.getElementById("cont-info-dcha");
+var row1 = document.getElementById("row1");
+var row2 = document.getElementById("row2");
 
-fetch('http://127.0.0.1:5000/static/json/peliculas.json')
-    .then(response => response.json())
-    .then(data => 
-        data.peliculas.forEach(peli => {
-            if (peli.id == idPelicula) {
-                contInfoDcha.innerHTML += `
-                    <img src="${peli.poster}" alt="Poster de pelicula: ${peli.title}"> `
+fetch("http://127.0.0.1:5000/static/json/peliculas.json")
+  .then((response) => response.json())
+  .then((data) =>
+    data.peliculas.forEach((peli) => {
+      if (peli.id == idPelicula) {
+        contInfoDcha.innerHTML += `
+                    <img src="${peli.poster}" class="poster" alt="Poster de pelicula: ${peli.title}"> `;
 
-                row1.innerHTML += `
+        row1.innerHTML += `
                     <h2>${peli.title}</h2>
                     <div>
-                        <p>${peli.date} /</p>
-                        <p>${peli.genre}</p>
-                    </div> `
+                        <p>Fecha de estreno: ${peli.date} </p>
+                        <p>Genero: <b> ${peli.genre} </b> </p>
+                    </div> `;
 
-                row2.innerHTML += `
+        row2.innerHTML += `
                     <h3>Sinopsis</h3>
-                    <p>${peli.overview}</p>
+                    <p class="sinopsis">${peli.overview}</p>
                     <h3>Direccion</h3>
-                    <p>${peli.director}</p> `
-            }    
-        })  
-    )
+                    <p>${peli.director}</p> 
+                    <h3>Trailer</h3>
+                    <iframe width="560" height="315" src="${peli.trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      }
+    })
+  );
