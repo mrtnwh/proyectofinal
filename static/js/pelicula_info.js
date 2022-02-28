f0e117f2f2cee1b859a4b6c9f4b8f742e74f6b02
tatianagutierrez <tatiana7gutierrez@gmail.com>
2022-02-28 10:49:51 -0300

    cambie ruta peliculas.json

--- static/js/pelicula_info.js
+++ static/js/pelicula_info.js
@@ -3,7 +3,7 @@ var contInfoDcha = document.getElementById("cont-info-dcha");
 var row1 = document.getElementById("row1");
 var row2 = document.getElementById("row2");
 
-fetch("http://127.0.0.1:5000/static/json/peliculas.json")
+fetch("/static/json/peliculas.json")
   .then((response) => response.json())
   .then((data) =>
     data.peliculas.forEach((peli) => {
